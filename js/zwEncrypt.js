994ec89af82004460948bf8ce1786d3381a79c61
Abzense <118556966+Abzense@users.noreply.github.com>
2023-02-19 23:29:36 -0700

    Update zwEncrypt.js

--- js/zwEncrypt.js
+++ js/zwEncrypt.js
@@ -38,16 +38,15 @@ function zwROT(input, ROT, encode){
         return output;
 }
 
-var Chart = "m,e.)(4cC:3012wqrliof89";
-
 function zwEncode(input, ROT, salt, bgSalt){
         var output = zwSalt(bgSalt);
         var newInput = zwROT(input, ROT % 26, true);
         var how = 0;
+	var Chart = "m,e.)(4cC:3012wqrliof89";
         for (var z = 0; z < newInput.length; z += 1){
                 output = output + newInput.substr(z, 1);
                 how = Math.floor(Math.random() * 4) + 5;
-                output = output + substr(Chart, how, 1) + zwSalt(how);
+                output = output + Chart.substr(how, 1) + zwSalt(how);
                 output = output + zwSalt(salt);
         }
         return output;
@@ -56,6 +55,7 @@ function zwEncode(input, ROT, salt, bgSalt){
 function zwDecode(input, ROT, salt, bgSalt){
         var blank = parseInt(bgSalt);
         var output = "";
+	var Chart = "m,e.)(4cC:3012wqrliof89";
         for(var a = 0; a < input.length; a += 1){
                 if(blank > 0){
                         blank -= 1;
